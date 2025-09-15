c3906ab5ecf514d9a34e1015408d39dc503e4b31
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-09-15 03:15:09 -0300

    switched from lists to sets to avoid repeated values, also fixed some type declarations that missed contained type

--- reader.py
+++ reader.py
@@ -1,7 +1,7 @@
 
-def readFile(filename: str ) -> list:
-    raw_array: list = []
-    cleaned_array: list = []
+def readFile(filename: str ) -> set[str]:
+    raw_array: list[str] = []
+    cleaned_array: set[str] = set()
     try:
         with open(filename, 'r') as repository:
             raw_array = repository.readlines()
@@ -10,19 +10,22 @@ def readFile(filename: str ) -> list:
     finally:
         for line in raw_array.copy():
             if line != 'Commander\n' and line != 'Deck\n' and line != '\n':
-                cleaned_array.append(line[1:-1])
-            
+                split_card: list = line.split()
+                split_card.pop(0)
+                final_card = ' '.join(split_card)
+                cleaned_array.add(final_card)
+                
         return cleaned_array
         
             
-def searchMatches(search_file: str, repository_file: str) -> list:
-        look_up: list = readFile(search_file)
-        repository: list = readFile(repository_file)
+def searchMatches(search_file: str, repository_file: str) -> set[str]:
+        look_up: set[str] = readFile(search_file)
+        repository: set[str] = readFile(repository_file)
 
-        matches: list = []
+        matches: set[str] = set()
 
         for card in look_up:
             if card in repository:
-                matches.append(card)
+                matches.add(card)
 
         return matches
