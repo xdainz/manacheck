8e736f09cc05df9c8e4476afd14d5c1cfa498eaa
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-10-29 19:48:22 -0300

    cleaner imports

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
-import fetcher, utils
+from utils import splash_screen, get_link, clean_data, get_matches
+from fetcher import get
 
 pink_start = '\033[35m'
 pink_end = '\033[0m'
 
 def main() -> None:
     # start screen
-    utils.splash_screen()
+    splash_screen()
     
     # data storer
     data_search: set = set() 
     data_repository: set = set()
 
     # ask user for links
-    search_link = utils.get_link('search')
-    repository_link = utils.get_link('repository')
+    search_link = get_link('search')
+    repository_link = get_link('repository')
 
     # append fetched data
-    for data in utils.clean_data(fetcher.get_manabox_content(search_link)):
+    for data in clean_data(get(search_link)):
         data_search.add(data)
     
-    for data in utils.clean_data(fetcher.get_manabox_content(repository_link)):
+    for data in clean_data(get(repository_link)):
         data_repository.add(data)
 
     # check matching data
-    final_matches: set[str] = utils.get_matches(data_search, data_repository)
+    final_matches: set[str] = get_matches(data_search, data_repository)
 
     if len(final_matches) >=1:
         print('\nMatches found:')
