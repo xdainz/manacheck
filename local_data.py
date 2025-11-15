d3fe28686d89441ee5c60b438f7e6762cd721dbd
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-11-14 23:23:15 -0300

    cast collector number data type on read to string to avoid data duplication when merging into db

--- local_data.py
+++ local_data.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-import fetcher
 
 DATABASE = 'data.xlsx'
 
@@ -28,18 +27,30 @@ def create_db() -> None:
         layout_df.to_excel(DATABASE, sheet_name='test', index=False)
 
 def write_db(card_list:list[dict]) -> None:
+    DTYPE_MAP = {
+        CARD_NAME: str,
+        SET: str,
+        COLLECTOR_NUMBER: str, # force data type to str on read
+        RARITY: str,
+        QUANTITY: int
+    }
+
     # turns card list into a dataframe 
     new_df = pd.DataFrame(card_list)
-    
+   
     # read existing db
-    db = pd.read_excel(DATABASE)
+    db = pd.read_excel(DATABASE, dtype=DTYPE_MAP)
     
     # merge new and old dataframes
     combined_df = pd.concat([db, new_df], ignore_index=True)
     
+    # headers that should match to increase quantity and avoid duplicates
+    unique_headers = [CARD_NAME, SET, COLLECTOR_NUMBER, RARITY]
+    
+    final_df = combined_df.groupby(unique_headers, as_index=False)[QUANTITY].sum()
+    
     # save new df to database file
-    combined_df.to_excel(DATABASE, index=False)
-
+    final_df.to_excel(DATABASE, index=False)
 
 def read_db():
     df = pd.read_excel(DATABASE)
@@ -47,6 +58,7 @@ def read_db():
 
 
 if __name__ == '__main__':
+    import fetcher
     create_db()
     write_db(fetcher.get('https://manabox.app/decks/vYzyl7ykTz6UtEkrlQB1bA'))
     print(read_db())
