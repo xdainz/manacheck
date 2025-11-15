a3bd05bbeae306722b97872dac25aa70122f0bc5
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-11-14 21:13:56 -0300

    refactor to use new fetch

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils import splash_screen, get_link, clean_data, get_matches
+from utils import splash_screen, get_link, get_matches
 from fetcher import get
 
 pink_start = '\033[35m'
@@ -8,23 +8,16 @@ def main() -> None:
     # start screen
     splash_screen()
     
-    # data storer
-    data_search: set = set() 
-    data_repository: set = set()
-
     # ask user for links
     search_link = get_link('search')
     repository_link = get_link('repository')
-
-    # append fetched data
-    for data in clean_data(get(search_link)):
-        data_search.add(data)
     
-    for data in clean_data(get(repository_link)):
-        data_repository.add(data)
-
+    # data storer
+    data_search: list[dict] = get(search_link) 
+    data_repository: list[dict] = get(repository_link)
+    
     # check matching data
-    final_matches: set[str] = get_matches(data_search, data_repository)
+    final_matches: list[str] = get_matches(data_search, data_repository)
 
     if len(final_matches) >=1:
         print('\nMatches found:')
