dc04a03d07847060dc651cc8105db9782d63e786
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-12-05 19:38:02 -0300

    home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import DeckFetcherDemo from "../components/DeckFetcherDemo";
+import Logo from "../components/Logo";
+
+function Home() {
+    return (
+        <div className="container home-page">
+            <Logo />
+            <DeckFetcherDemo />
+        </div>
+    );
+}
+
+export default Home;
