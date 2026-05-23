f8a7ec571327b128055cd6318ed10e9d06f44a10
dainz <mbenjaminarancibia@gmail.com>
2026-05-23 00:34:34 -0400

    fix url path

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Layout from "./layout/Layout";
 
 function App() {
     return (
-        <HashRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
             <Layout>
                 <Routes>
                     <Route path="/" element={<Home />} />
                 </Routes>
             </Layout>
-        </HashRouter>
+        </BrowserRouter>
     );
 }
 
