f0eab6326763f37ada118bab42f442301be8b6df
Matías Arancibia <88006301+xdainz@users.noreply.github.com>
2026-08-09 22:51:13 -0400

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -16,6 +16,6 @@ export const storeList: Store[] = [
         ck_price: 700,
         website: "https://www.facebook.com/profile.php?id=100049903494774",
         image_banner:
-            "https://scontent.fscl7-1.fna.fbcdn.net/v/t39.30808-6/358667749_800153914991384_4851040867091623483_n.jpg?stp=dst-jpg_tt6&cstp=mx960x950&ctp=s960x950&_nc_cat=105&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=Xx0rKAVXcG8Q7kNvwG1K8Qb&_nc_oc=AdoYQuytfmn__ldkAvX-sPx2_RlW6SLzOiTXkc2LwbH5bBaYsphJnfaSlq-AT1_19JA&_nc_zt=23&_nc_ht=scontent.fscl7-1.fna&_nc_gid=mTqZ85TTmuQvkxcZ1wa3FA&_nc_ss=7b2a8&oh=00_AQEb8omFiMbwBRJsrbuVjdfw8N3s4RybL_40c5FLEV8-ZQ&oe=6A7750B6",
+            "https://kappa.lol/vEGx4M",
     },
 ];
